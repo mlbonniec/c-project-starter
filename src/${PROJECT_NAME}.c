8f2a0e1a3bd6a71a43cee84032f59a56e71b8501
Mathis Le Bonniec <mathis.le-bonniec@epitech.eu>
2021-12-21 22:30:32 +0100

    feat(src): add main project file

---
+++ src/${PROJECT_NAME}.c
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2021
+** ${PROJECT_NAME}
+** File description:
+** ${PROJECT_NAME} main file
+*/
+
+#include "${PROJECT_NAME}"
+
+int main(int ac, char **av)
+{
+    return 0;
+}
