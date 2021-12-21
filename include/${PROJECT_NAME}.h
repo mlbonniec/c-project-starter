b28b501ed4bf32fe5e42dea429bb0ebf48d55cdc
Mathis Le Bonniec <mathis.le-bonniec@epitech.eu>
2021-12-21 22:30:06 +0100

    feat(headers): add main header file

---
+++ include/${PROJECT_NAME}.h
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2021
+** ${PROJECT_NAME}.h
+** File description:
+** ${PROJECT_NAME} header file
+*/
+
+#ifndef ${PROJECT_NAME_UPPERCASE}_H_
+    #define ${PROJECT_NAME_UPPERCASE}_H_
+
+
+
+#endif
