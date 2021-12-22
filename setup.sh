16553302c275e016bd96d36c6810bbc08bbba049
Mathis Le Bonniec <mathis.le-bonniec@epitech.eu>
2021-12-22 21:48:51 +0100

    feat(setup.sh): add more functionalities User is ask for the remote URL File can be deleted after setup Setup can't be done twice

--- setup.sh
+++ setup.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
+already_setup=false
+
 # If no args is passed, exit with an error
 if [ -z "$1" ]; then
     echo "You must specify a project name."
     exit 1
+elif $already_setup; then
+    echo "You already have setup this project."
+    exit 1
 fi
 
-cmds=(find rename sed)
+# Check if used commands exists
+cmds=(find rename sed rm)
 for cmd in "${cmds[@]}"
 do
     if ! [ -x "$(command -v $cmd)" ]; then
@@ -16,7 +22,29 @@ do
     fi
 done
 
-find . -name "*\${PROJECT_NAME}*" -exec rename "\${PROJECT_NAME}" "${1,,}" {} ";"
+# Ask for Github repository name to setup remote
+read -p "Please specify the Github repository name: " repository
+if [ -z "$repository" ]; then
+    echo "You must specify the Github repository name."
+    exit 1
+else
+    read -p "Validate repository name: $repository (yes/no): " validation
+    # TODO: setup git remote url
+    if [ "$validation" == "no" ] || [ "$validation" == "n" ]; then
+        echo "Setup aborted."
+        exit 1
+    fi
+fi
+
+# Replace file names and content with project name
+find . -name "*\${PROJECT_NAME}*" -exec rename "\${PROJECT_NAME}" "${1,,}" {} ";" \
+&& echo "Successful renaming files." || echo "Error in renaming files"
 find . -type f -exec sed -i "s/\${PROJECT_NAME}/${1,,}/g" {} +
 find . -type f -exec sed -i "s/\${PROJECT_NAME_UPPERCASE}/${1^^}/g" {} +
-exit 0
+sed -i 's/already_setup=false/already_setup=true/g' setup.sh
+
+read -p "Would you delete this setup file? (yes/no): " deletion
+if [ "$deletion" == "yes" ] || [ "$deletion" == "y" ]; then
+    echo "File deleted."
+    rm setup.sh
+fi
